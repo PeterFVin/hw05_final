9e6df67aec3d1d078b83b2ff901b5396bd0ef8cb
Петр Виноградов <petrowaw@yandex.ru>
2023-04-20 23:51:21 +0300

    rev. 6.2

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -130,6 +130,8 @@ def profile_follow(request, username):
 
 @login_required
 def profile_unfollow(request, username):
-    follow = get_object_or_404(Follow, user=request.user, author__username=username)
+    follow = get_object_or_404(
+        Follow, user=request.user, author__username=username
+    )
     follow.delete()
     return redirect('posts:profile', username=username)
